e4eef2ffac408575b233172ce336951c0a8c93a0
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-11-26 22:22:06 +0330

    add user domain to the project

---
+++ internal/domain/user.go
@@ -0,0 +1,12 @@
+package domain
+
+import "time"
+
+type User struct {
+	Id        string    `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
